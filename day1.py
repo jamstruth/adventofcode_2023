5cd0798dc2360f7d702bfbdf5fae6e4b81366a75
Jamstruth <jamstruth18@gmail.com>
2023-12-06 22:33:00 +0000

    Updating D1 to try and parse with Regex FOR SOME REASON IT GETS EMPTY STRINGS INSTEAD OF DIGITS NOW

--- day1.py
+++ day1.py
@@ -1,16 +1,44 @@
 import sys
 import re
 
+from numpy import number
+
 file_path = sys.argv[1]
 
-# Open file
-f = open(file_path)
 
-total = 0
-for line in f.readlines():
-    # filter the line to only numbers
-    all_numbers = re.findall(r'\d', line)
-    number = int(all_numbers[0] + all_numbers[-1])
-    total = total + number
+
+class Calculator:
     
-print(total)
+    def __init__(self, file_path):
+        self.file_path = file_path
+        
+    def calculate_file(self):
+        # Open file
+        with open(file_path) as f:
+            total = 0
+            for line in f.readlines():
+                total = total + self.calculate_line(line)
+        return total
+    
+    DIGIT_WORDS = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+    DIGIT_WORDS_REGEX = '|'.join(DIGIT_WORDS)
+    
+    FULL_REGEX = fr'({DIGIT_WORDS_REGEX})|\d'
+    
+    def calculate_line(self, line):
+        # filter the line to only numbers
+        all_digits = re.findall(self.FULL_REGEX, line)
+        print(self.FULL_REGEX)
+        print(all_digits)
+        first_digit = self.get_numeric_string(all_digits[0])
+        last_digit = self.get_numeric_string(all_digits[-1])
+        return int(first_digit + last_digit)
+    
+    def get_numeric_string(self, number_string):
+        if number_string.isnumeric():
+            return number_string 
+        return self.DIGIT_WORDS.index(number_string)
+        
+        
+    
+print(Calculator(file_path).calculate_file())
